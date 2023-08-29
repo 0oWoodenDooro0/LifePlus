5405e888507e8dab7977ee023761cfdf66209d92
0oWoodenDooro0 <vincent031525@gmail.com>
2023-08-29 20:09:58 +0800

    Add Video prepare image

--- app/src/main/java/com/example/lifeplus/ui/VideoDialog.kt
+++ app/src/main/java/com/example/lifeplus/ui/VideoDialog.kt
@@ -1,22 +1,23 @@
 package com.example.lifeplus.ui
 
 import android.net.Uri
-import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
-import android.widget.FrameLayout
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.basicMarquee
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
@@ -28,6 +29,7 @@ import androidx.media3.datasource.DefaultDataSource
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.exoplayer.source.ProgressiveMediaSource
 import androidx.media3.ui.PlayerView
+import com.example.lifeplus.R
 import com.example.lifeplus.domain.VideoData
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -57,6 +59,8 @@ fun VideoDialog(onDismiss: () -> Unit, videoData: VideoData) {
 @Composable
 @androidx.annotation.OptIn(androidx.media3.common.util.UnstableApi::class)
 fun VideoPlayer(uri: Uri) {
+    val isPrepared = remember { mutableStateOf(false) }
+
     val context = LocalContext.current
     val player = ExoPlayer.Builder(context).build()
     val playerView = PlayerView(context)
@@ -69,21 +73,31 @@ fun VideoPlayer(uri: Uri) {
     val source =
         ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(MediaItem.fromUri(uri))
 
+    playerView.requestFocus()
     playerView.hideController()
     playerView.useController = false
     player.setMediaSource(source)
     player.repeatMode = Player.REPEAT_MODE_ONE
+    player.playWhenReady = true
     playerView.player = player
     LaunchedEffect(player) {
         player.prepare()
-        player.play()
+        isPrepared.value = true
     }
-    AndroidView(
-        modifier = Modifier
-            .wrapContentSize()
-            .padding(5.dp),
-        factory = {
-            playerView.apply { layoutParams = FrameLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT) }
-        }
-    )
+    if (isPrepared.value) {
+        AndroidView(
+            modifier = Modifier
+                .wrapContentHeight()
+                .padding(5.dp),
+            factory = {
+                playerView
+            }
+        )
+    } else {
+        Image(
+            painter = painterResource(id = R.drawable.placeholder),
+            contentDescription = "loading image",
+            modifier = Modifier.wrapContentHeight()
+        )
+    }
 }
